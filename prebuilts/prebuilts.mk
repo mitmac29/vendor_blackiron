123c56f6ebcf99f9f14110fa40f9edbcb43ba41e
ralf979 <ralf979@gmail.com>
2024-11-04 21:52:27 +0100

    Revert "Introduce bootanimation styles [3/3]"
    This reverts commit d43d97bf51ece3d452603cf04c3a579db45f060c.

--- prebuilts/prebuilts.mk
+++ prebuilts/prebuilts.mk
@@ -48,9 +48,4 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
 endif
 endif
 
-PRODUCT_COPY_FILES += \
-    vendor/blackiron/prebuilts/bootanimation/bootanimation_blackiron.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_blackiron.zip \
-    vendor/blackiron/prebuilts/bootanimation/bootanimation_cyberpunk.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_cyberpunk.zip \
-    vendor/blackiron/prebuilts/bootanimation/bootanimation_valorant.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_valorant.zip \
-    vendor/blackiron/prebuilts/bootanimation/bootanimation_google.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_google.zip \
-    vendor/blackiron/prebuilts/bootanimation/bootanimation_google_monet.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation_google_monet.zip
+PRODUCT_COPY_FILES += vendor/blackiron/prebuilts/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
