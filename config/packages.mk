cb0f1f1aaea4037f87c949dc8666e0567b2bf3ab
Pranav Vashi <neobuddy89@gmail.com>
2024-12-04 11:09:13 +0100

    blackiron: Adding an option to build DeviceAsWebcam

--- config/packages.mk
+++ config/packages.mk
@@ -18,3 +18,11 @@ ifneq ($(WITH_GMS),true)
 PRODUCT_PACKAGES += \
     LatinIME
 endif
+
+# DeviceAsWebcam
+ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
+    PRODUCT_PACKAGES += \
+        DeviceAsWebcam
+    PRODUCT_VENDOR_PROPERTIES += \
+        ro.usb.uvc.enabled=true
+endif
